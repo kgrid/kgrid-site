4c4c625237da313f4a53aac4e8b5efd6bb3a7320
Jack Allan <jackall@umich.edu>
2019-02-21 11:47:56 -0500

    switch to vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  base: '/ipp-collection/',
+  title: 'ipp Collection',
+  themeConfig: {
+    repo: 'kgrid-objects/ipp-collection',
+    lastUpdated: 'Last Updated',
+    nav: [
+      { text: 'KGrid.org', link: 'https://kgrid.org' },
+      { text: 'Guide', link: '/' },
+      // { text: 'IPP Kit', link: 'https://demo.kgrid.org/ipp-kit'},
+      // { text: 'Online Demo', link: 'https://demo.kgrid.org/ipp-kit/web' }
+    ],
+    search: true,
+    searchMaxSuggestions: 10,
+    sidebar: 'auto',
+    displayAllHeaders: true
+  }
+}
